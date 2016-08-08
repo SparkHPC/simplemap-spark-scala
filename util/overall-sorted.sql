caf39773eecd3062cdcf049ed5a38b53e5f83566
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-08-08 03:17:50 +0000

    update initial report and add dataset query code
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ util/overall-sorted.sql
@@ -0,0 +1,10 @@
+.mode csv
+.header on
+
+select (nodes*blocks*block_size)/1024 as sum_gb,
+       (nodes*cores*nparts) as partitions,
+       nodes,blocks,block_size,
+       map_time/1E9 as map_time_sec,
+       shift_time/1E9 as shift_time_sec
+       from results
+       order by sum_gb,nodes,cores,blocks,block_size;
