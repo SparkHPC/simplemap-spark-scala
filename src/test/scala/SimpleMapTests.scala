0017fc48bb9b95761bf40a167ae15bad59401453
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-08-11 21:03:57 -0500

    unit tests for command-line handling
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- src/test/scala/SimpleMapTests.scala
+++ src/test/scala/SimpleMapTests.scala
@@ -3,7 +3,23 @@ import org.scalatest._
 import breeze.linalg._
 import edu.luc.cs.SimpleMap._
 
-class SimpleMapTests extends FlatSpec with Matchers {
+/*
+
+Initial efforts to test RDDs. Doesn't seem to work with all ScalaTest frameworks.
+
+import com.holdenkarau.spark.testing._
+import org.scalatest.Assertions._
+
+class SampleRDD extends FunSuite with SharedSparkContext {
+  test("really simple transformation") {
+    val input = List("hi", "hi cloudera", "bye")
+    val expected = List(List("hi"), List("hi", "cloudera"), List("bye"))
+
+  }
+}
+*/
+
+class SimpleMapCoreTests extends FlatSpec with Matchers {
 
   "Breeze Basics" should "work as expected" in {
     val a = Array(1.0, 2.0, 3.0)
@@ -18,5 +34,19 @@ class SimpleMapTests extends FlatSpec with Matchers {
       data(i).rows should be(MB_OF_FLOATS)
       data(i).cols should be(3)
     }
+  }
+
+  "parseCommandLine()" should "generate valid config" in {
+    val args = "--generate --blocks 1 --block_size 10 --nodes 4 --nparts 10 --cores 12 --json output.json --xml output.xml".split(" ")
+    val config = parseCommandLine(args).get
+    config.generate should be(true)
+    config.blocks should be(1)
+    config.blockSize should be(10)
+    config.nodes should be(4)
+    config.cores should be(12)
+    config.jsonFilename.get should be("output.json")
+    config.xmlFilename.get should be("output.xml")
+    config.src should be(None)
+    config.dst should be(None)
   }
 }
