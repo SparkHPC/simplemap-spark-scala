c041d40252d807c065695c9e1e83a385a120c63a
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-08-24 22:16:05 -0500

    significant cleanup of plotting code
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- util/plot-from-db.py
+++ util/plot-from-db.py
@@ -1,72 +1,75 @@
-# 
-# Note: Proof of concept. Not in any way ready for primetime!
 #
+# Plot strong, weak, and data scaling data from performance results, which
+# are collected in an ephemeral sqlite3 database.
+# 
 
-import sqlite3
 import sys
-
+import sqlite3
 
 def dict_factory(cursor, row):
-  d = {}
+  record = {}
   for idx, col in enumerate(cursor.description):
-    d[col[0]] = row[idx]
-  return d
+    record[col[0]] = row[idx]
+  return record
 
-Q="select distinct(block_size) from results"
+def linestyle_iterator():
+  for (color, shape) in zip("bgrcmyk"*100, "o^*8s"*100):
+    yield color + shape + "--"
 
-conn = sqlite3.connect("simplemap-spark-scala.db")
-conn.execute(Q)
+class PerfReport(object):
+  def __init__(self):
+    self.conn = sqlite3.connect("simplemap-spark-scala.db")
+    self.conn.row_factory = dict_factory
 
-conn.row_factory = dict_factory
+  def block_sizes_iterator(self):
+    query = "select distinct(block_size) from results"
+    records = self.conn.execute(query)
+    for r in records:
+      yield r['block_size']
 
-cursor = conn.execute(Q)
+  def get_block_sizes(self):
+    block_sizes = list(self.block_sizes_iterator())
+    block_sizes.sort()
+    return block_sizes
 
-block_sizes = [ b['block_size'] for b in cursor]
-block_sizes.sort()
-print(block_sizes)
+  def get_by_blocksize(self):
+    query = """select * from results
+               where block_size = %(block_size)d
+                 and nodes * cores * nparts == blocks and nparts=1
+               order by nodes"""
 
-Q2 = "select * from results where block_size = %(b)d and nodes * cores * nparts == blocks and nparts=1 order by nodes"
+    by_blocksize = {}
+    for block_size in self.get_block_sizes():
+	cursor = self.conn.execute(query % vars())
+	by_blocksize[block_size] = [
+         { 'nodes' : result['nodes'],
+           'map_time' : result['map_time'] / 1.0e9,
+           'shift_time' : result['shift_time'] / 1.0e9,
+           'avg_time' : result['shift_time'] / 1.0e9
+         } for result in cursor ]
+    return by_blocksize
 
-by_block = {}
+  def create_report(self, name):
+    import matplotlib.pyplot as plt
+    plt.xlabel('Nodes')
+    plt.ylabel('Time (seconds)')
+    plt.title("Performance (%s) by Nodes, Block Size (x Nodes x Cores/Node)" % name)
+    linestyle = linestyle_iterator()
+    legendlines = []
+    by_block = self.get_by_blocksize()
+    for b in self.get_block_sizes():
+      nodes = [ d['nodes'] for d in by_block[b]]
+      t = [ d[name] for d in by_block[b]]
+      line, = plt.plot(nodes, t, linestyle.next(), label="%(b)d MB" % vars(), linewidth=1)
 
-# TODO: Need to get a list of known linestyles w/o hard coding them.
+      legendlines.append(line)
+      plt.legend(handles=legendlines, loc=1)
+      #plt.axis([0, 6, 0, 20])
 
-# This just generates different line color/shapes for each line being ploted.
+    plt.savefig('%s.png' % name)
+    plt.close()
 
-def linestyle_gen():
-   for (color, shape) in zip("bgrcmyk"*100, "o^*8s"*100):
-      yield color + shape + "--"
-
-for b in block_sizes:
-	Q3=Q2 % vars()
-	cursor = conn.execute(Q3)
-	by_block[b] = [
-    { 'nodes' : result['nodes'],
-      'map_time' : result['map_time'] / 1.0e11,
-      'shift_time' : result['shift_time'] / 1.0e11,
-      'avg_time' : result['shift_time'] / 1.0e11
-    } for result in cursor]
-  #print("Number of items in %d = %d" % (b, len(by_block[b])))
-
-def create_report(name):
-  import matplotlib.pyplot as plt
-  plt.xlabel('Nodes')
-  plt.ylabel('Time (x10^11)')
-  plt.title("Performance (%s) by Nodes, Block Size (x Nodes x Cores/Node)" % name)
-  linestyle = linestyle_gen()
-  legendlines = []
-  for b in block_sizes:
-    nodes = [ d['nodes'] for d in by_block[b]]
-    t = [ d[name] for d in by_block[b]]
-    line, = plt.plot(nodes, t, linestyle.next(), label="%(b)d MB" % vars(), linewidth=1)
-
-    legendlines.append(line)
-    plt.legend(handles=legendlines, loc=1)
-    #plt.axis([0, 6, 0, 20])
-
-  plt.savefig('%s.png' % name)
-  plt.close()
-
-create_report('map_time')
-create_report('avg_time')
-create_report('shift_time')
+p = PerfReport()
+p.create_report('map_time')
+p.create_report('avg_time')
+p.create_report('shift_time')
