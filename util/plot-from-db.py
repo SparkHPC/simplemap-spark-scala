d045911e49a81df2139ee5ad0c27eecffff57e51
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-09-02 21:53:42 -0500

    updates for plotting strong scaling experiments
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- util/plot-from-db.py
+++ util/plot-from-db.py
@@ -1,8 +1,3 @@
-#
-# Plot strong, weak, and data scaling data from performance results, which
-# are collected in an ephemeral sqlite3 database.
-# 
-
 import sys
 import sqlite3
 
@@ -21,43 +16,67 @@ class PerfReport(object):
     self.conn = sqlite3.connect("simplemap-spark-scala.db")
     self.conn.row_factory = dict_factory
 
-  def block_sizes_iterator(self):
-    query = "select distinct(block_size) from results"
+  def distinct_blocks_iterator(self):
+    query = "select distinct(blocks) from results order by blocks"
     records = self.conn.execute(query)
     for r in records:
-      yield r['block_size']
+      yield r['blocks']
 
-  def get_block_sizes(self):
-    block_sizes = list(self.block_sizes_iterator())
-    block_sizes.sort()
-    return block_sizes
+  def get_distinct_blocks(self):
+    return list(self.distinct_blocks_iterator())
 
-  def get_by_blocksize(self):
+  def distinct_gb_iterator(self):
+    query = "select distinct(blocks * block_size / 1024) as gb from results order by gb"
+    records = self.conn.execute(query)
+    for r in records:
+      yield r['gb']
+    
+  def get_distinct_gb(self):
+    return list(self.distinct_gb_iterator())
+
+  def get_perf_by_blocks(self):
     query = """select * from results
-               where block_size = %(block_size)d
-                 and nodes * cores * nparts == blocks and nparts=1
+               where blocks = %(blocks)d and nparts=4
                order by nodes"""
 
-    by_blocksize = {}
-    for block_size in self.get_block_sizes():
+    by_blocks = {}
+    for blocks in self.get_distinct_blocks():
 	cursor = self.conn.execute(query % vars())
-	by_blocksize[block_size] = [
+	by_blocks[blocks] = [
          { 'nodes' : result['nodes'],
            'map_time' : result['map_time'] / 1.0e9,
            'shift_time' : result['shift_time'] / 1.0e9,
            'avg_time' : result['shift_time'] / 1.0e9
          } for result in cursor ]
-    return by_blocksize
+    return by_blocks
 
-  def create_report(self, name):
+  def get_perf_by_gb(self):
+    query = """select blocks * block_size / 1024 as gb, nodes, map_time, shift_time, avg_time from results
+               where gb = %(gb)d and nparts=4
+               order by nodes"""
+
+    by_gb = {}
+    for gb in self.get_distinct_gb():
+        cursor = self.conn.execute(query % vars())
+        by_gb[gb] = [
+         { 'gb' : result['gb'], 
+           'nodes' : result['nodes'],
+           'map_time' : result['map_time'] / 1.0e9,
+           'shift_time' : result['shift_time'] / 1.0e9,
+           'avg_time' : result['shift_time'] / 1.0e9
+         } for result in cursor ]
+    return by_gb
+
+  def create_weak_report(self, name):
     import matplotlib.pyplot as plt
     plt.xlabel('Nodes')
     plt.ylabel('Time (seconds)')
-    plt.title("Performance (%s) by Nodes, Block Size (x Nodes x Cores/Node)" % name)
+    plt.yscale('log')
+    plt.title("Weak Scaling (%s)" % name)
     linestyle = linestyle_iterator()
     legendlines = []
-    by_block = self.get_by_blocksize()
-    for b in self.get_block_sizes():
+    by_block = self.get_perf_by_blocks()
+    for b in self.get_distinct_blocks():
       nodes = [ d['nodes'] for d in by_block[b]]
       t = [ d[name] for d in by_block[b]]
       line, = plt.plot(nodes, t, linestyle.next(), label="%(b)d MB" % vars(), linewidth=1)
@@ -66,10 +85,36 @@ class PerfReport(object):
       plt.legend(handles=legendlines, loc=1)
       #plt.axis([0, 6, 0, 20])
 
-    plt.savefig('%s.png' % name)
+    plt.savefig('weak_scaling_%s.png' % name)
+    plt.close()
+
+  def create_strong_report(self, name):
+    import matplotlib.pyplot as plt
+    plt.xlabel('Nodes')
+    plt.ylabel('Time (seconds)')
+    plt.yscale('log')
+    plt.title("Strong Scaling (%s)" % name)
+    linestyle = linestyle_iterator()
+    legendlines = []
+    by_gb = self.get_perf_by_gb()
+    for b in self.get_distinct_gb():
+      nodes = [ d['nodes'] for d in by_gb[b]]
+      t = [ d[name] for d in by_gb[b]]
+      line, = plt.plot(nodes, t, linestyle.next(), label="%(b)d MB" % vars(), linewidth=1)
+
+      legendlines.append(line)
+      plt.legend(handles=legendlines, loc=1)
+      #plt.axis([0, 6, 0, 20])
+
+    plt.savefig('strong_scaling_%s.png' % name)
     plt.close()
 
 p = PerfReport()
-p.create_report('map_time')
-p.create_report('avg_time')
-p.create_report('shift_time')
+p.create_weak_report('map_time')
+p.create_weak_report('avg_time')
+p.create_weak_report('shift_time')
+
+p.create_strong_report('map_time')
+p.create_strong_report('avg_time')
+p.create_strong_report('shift_time')
+
