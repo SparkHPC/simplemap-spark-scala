d134f7a3583feb63f2a932021d8cd972a82f0a31
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-09-02 21:55:48 -0500

    fix query to report correct aggregate data size
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- util/overall-sorted.sql
+++ util/overall-sorted.sql
@@ -1,7 +1,7 @@
 .mode tabs
 .header on
 
-select (nodes*blocks*block_size)/1024 as gb,
+select (blocks*block_size)/1024 as gb,
        (nodes*cores*nparts) as nparts,
        nodes,blocks,block_size as blk_sz,
        map_time/1E9 as rdd,
