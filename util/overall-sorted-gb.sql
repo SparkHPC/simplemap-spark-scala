3380a04e6851b7bfd102e394a68a6fd1c7d6f564
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-09-02 21:54:58 -0500

    shows how to use calulated column for reporting by aggregate data size
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ util/overall-sorted-gb.sql
@@ -0,0 +1,12 @@
+.mode tabs
+.header on
+
+select (blocks*block_size)/1024 as gb,
+       (nodes*cores*nparts) as nparts,
+       nodes,blocks,block_size as blk_sz,
+       map_time/1E9 as rdd,
+       shift_time/1E9 as rdd_shift,
+       avg_time/1E9 as rdd_avg
+       from results
+       where nparts=4
+       order by gb,nodes,cores,blocks,block_size;
