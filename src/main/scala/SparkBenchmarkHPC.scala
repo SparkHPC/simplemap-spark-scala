a2f17fd06c69af334f13911918e4b8ab9dcd1caf
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-07-03 16:22:31 +0000

    hopefully restored to normal

--- src/main/scala/SparkBenchmarkHPC.scala
+++ src/main/scala/SparkBenchmarkHPC.scala
@@ -265,8 +265,8 @@ object SparkBenchmarkHPC {
     }
 
     def toJSON(): org.json4s.JsonAST.JObject = {
-      val timeData = ("mapTime" -> mapTime.toString) ~ ("shiftTime" -> shiftTime.toString) ~ ("avgTime" -> avgTime.toString) ~ ("reduceTime" -> reduceTime.toString) ~ ("report" -> timeData)
-      timeData
+      val timeData = ("mapTime" -> mapTime.toString) ~ ("shiftTime" -> shiftTime.toString) ~ ("avgTime" -> avgTime.toString) ~ ("reduceTime" -> reduceTime.toString)
+      ("report" -> timeData)
     }
   }
 
