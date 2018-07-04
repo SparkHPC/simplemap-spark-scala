b5b26ac098b088ee35c33ea04bf26551795ee473
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-07-04 04:15:43 +0000

    add simple kernel benchmarks to understand baseline Scala/breeze performance

---
+++ src/main/scala/KernelBenchmark.scala
@@ -0,0 +1,38 @@
+/*
+ * SimpleMap: Large array tests with RDDs using Breeze DenseMatrix[Double] 
+ * for the block representation of array of (x, y, z) data. We use Array
+ * of DenseMatrix[Double] to scale up the data size, since Java arrays (and
+ * Scala by association) are presently limited to 32-bit size.
+ *
+ * Use --blocks parameter to multiply --block_size to get bigger arrays!
+ */
+package dataflows.spark
+
+import blockperf._
+import java.io._
+import breeze.linalg._
+import SparkBenchmarkHPC._
+
+object KernelBenchmark {
+
+  def main(args: Array[String]) {
+    val config = parseCommandLine(args).getOrElse(Config())
+
+    val (generateTime, _, array) = performance {
+      generate(0, config.blockSize)
+    }
+
+    val (shiftTime, _, shifted) = performance {
+      val shift = DenseVector(25.25, -12.125, 6.333)
+      addVectorDisplacement(array, shift)
+    }
+
+    val (avgTime, _, c) = performance {
+      averageOfVectors(shifted)
+    }
+
+    printf("generation: %f\n", generateTime.t / 1e9)
+    printf("shift: %f\n", shiftTime.t / 1e9)
+    printf("average: %f\n", avgTime.t / 1e9)
+  }
+}
