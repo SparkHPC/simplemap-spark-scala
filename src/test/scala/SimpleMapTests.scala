d92c0f41c1f16a7f6b5b80eaddf92ab66a981208
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-08-03 03:38:39 +0000

    unit test fixes

--- src/test/scala/SimpleMapTests.scala
+++ src/test/scala/SimpleMapTests.scala
@@ -21,36 +21,32 @@ class SampleRDD extends FunSuite with SharedSparkContext {
 
 class SparkBechmarkHPCTests extends FlatSpec with Matchers {
 
-  "Floating point tests" should "support tolerance of..." in {
-    val x = 3.0
-    val y = 2.0
-
-    x / y should be(1.5 +- 0.0001)
-  }
-
-  "Breeze Basics" should "work as expected" in {
+  "Breeze Kernel" should "initialize from Scala Array" in {
     val a = Array(1.0, 2.0, 3.0)
     val dv = DenseVector(a)
     dv should be(DenseVector(1.0, 2.0, 3.0))
   }
 
-  "SimpleMap" should "generate wrapped matrices" in {
-    val data = generate(1, 3)
-    data.length should be(3)
+  "SparkBenchmarkHPC" should "generate wrapped 32-bit arrays" in {
+    val args = "--generate --blocks 1 --block_size 10 --multiplier 16384".split(" ")
+    val config = parseCommandLine(args).get
+    val data = generate(1, config.blockSize, config.multiplier)
+    data.length should be(config.blockSize)
     for (i <- 0 until data.length) {
-      data(i).rows should be(MEGA_MULTIPLIER)
+      data(i).rows should be(config.multiplier)
       data(i).cols should be(3)
     }
   }
 
-  "parseCommandLine()" should "generate valid config" in {
-    val args = "--generate --blocks 1 --block_size 10 --nodes 4 --nparts 10 --cores 12 --json output.json --xml output.xml".split(" ")
+  it should "parse command line" in {
+    val args = "--generate --blocks 1 --block_size 10 --multiplier 16384 --nodes 4 --nparts 10 --cores 12 --json output.json --xml output.xml".split(" ")
     val config = parseCommandLine(args).get
     config.generate should be(true)
     config.blocks should be(1)
     config.blockSize should be(10)
     config.nodes should be(4)
     config.cores should be(12)
+    config.multiplier should be(16384)
     config.jsonFilename.get should be("output.json")
     config.xmlFilename.get should be("output.xml")
     config.src should be(None)
@@ -59,7 +55,7 @@ class SparkBechmarkHPCTests extends FlatSpec with Matchers {
 
   // TODO: Might rework this using ScalaCheck (if worth it)
 
-  "RandomDoubles" should "generate distribution of doubles across range" in {
+  "Scala" should "generate distribution of doubles across range" in {
     val gen = RandomDoubles(0, -3, 3)
     val data = Array.fill(10000)(gen.next)
     for (bucket <- -2 to 2) {
